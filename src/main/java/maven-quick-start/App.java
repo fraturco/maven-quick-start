b31f515fe3d67020bbe9fe5045b977b2bd544b43
Francesca Turco£git config --global user.name Francesca <Francesca>
2021-04-13 17:08:48 +0200

    modifica c

--- src/main/java/maven-quick-start/App.java
+++ src/main/java/maven-quick-start/App.java
@@ -1,13 +1,28 @@
-package maven-quick-start;
+/*  package maven-quick-start;
 
-/**
+
  * Hello world!
  *
- */
-public class App 
+
+public class App
 {
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+}*/
+
+package clinic.programming.training;
+
+public class Application {
+
+    public Application() {
+        System.out.println ("Inside Application");
+    }
+
+    // method main(): ALWAYS the APPLICATION entry point
+    public static void main (String[] args) {
+    	System.out.println ("Starting Application");
+	Application app = new Application();
     }
 }
