2250001ca0fcee1fdc2f39b436199b39b14485bc
Francesca Turco£git config --global user.name Francesca <Francesca>
2021-04-14 16:41:23 +0200

    add3

--- src/main/java/maven-quick-start/App.java
+++ src/main/java/maven-quick-start/App.java
@@ -2,10 +2,19 @@ package maven-quick-start;
 
  /* Hello world!
  */
+import java.util.ArrayList;
+import java.util.List;
 
 public class App {
+ public void greet (){
+    List<String> greetings = new ArrayList<>();
+    greetings.add("Hello");
 
-    public static void main( String[] args ){
+    for (String greeting : greetings) {
+      System.out.println("inside Application");
+    }
+ }
+  public static void main( String[] args ){
 
         System.out.println( "Hello World!" );
     }
